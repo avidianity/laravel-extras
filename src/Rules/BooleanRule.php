ad833231c2cd34150982db5c1006a3a32e34e82d
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2022-07-03 13:27:46 +0800

    FIX: boolean rule

--- src/Rules/BooleanRule.php
+++ src/Rules/BooleanRule.php
@@ -8,7 +8,15 @@ class BooleanRule implements Rule
 {
     public function passes($attribute, $value)
     {
-        return toBoolean($value);
+        if (is_bool($value)) {
+            return $value;
+        }
+
+        if (is_string($value) || is_numeric($value)) {
+            return in_array($value, ['true', 'false', 1, 0, '1', '0']);
+        }
+
+        return false;
     }
 
     public function message()
